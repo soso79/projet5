b5a4806d0243c3dedf019d1274752e5dd360ccdd
soso <succo13@yahoo.fr>
2018-05-22 17:21:03 +0200

    Everything working good , miss just landscape orientation

--- instagrid/Controller/ViewController.swift
+++ instagrid/Controller/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController,
     UIImagePickerControllerDelegate,
-    UINavigationControllerDelegate
+    UINavigationControllerDelegate,UIActionSheetDelegate
+    
     
 {
-    var myVc: UIViewController?
+    // outlet collection boutton layout
+    @IBOutlet var layOutButtonCollection: [UIButton]!
     var icon: UIImage!
     var currentButton: UIButton?
     @IBOutlet weak var photoView: PhotoView!
@@ -33,14 +35,17 @@ class ViewController: UIViewController,
     }
     
     // bouton pour changer le layout
-    
     @IBAction func didTapLayoutButton(_ sender: UIButton!) {
-        if sender.isSelected {
-            sender.isSelected = false
-        }else {
-            sender.isSelected = true
-            
+       // permet d afficher le signe seulement sur le bouton selectionné
+        for button in self.layOutButtonCollection{
+            if button.tag == sender.tag{
+                button.isSelected = true;
+                
+            }else{
+                button.isSelected = false
+            }
         }
+        // change le layout
         switch sender.tag {
         case 0:
             changeLayout(style: .layout1)
@@ -50,50 +55,34 @@ class ViewController: UIViewController,
             changeLayout(style: .layout3)
             
         default: break
+            
         }
-        
-        
         
         sender.setImage(#imageLiteral(resourceName: "icon"), for: .selected)
         
-        
     }
-    
-    
-    
     
     private func changeLayout( style: PhotoView.Style){
         photoView.style = style
     }
-    // Mise en place de UIaction mais ne fonctionne pas
-    @IBAction func openButtonLibrairy(_ sender: UIButton!) {
+    
+    
+    
+    func openButtonLibrairy(_ sender: UIButton!) {
 
-        let actionsheet = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = .photoLibrary;
+                imagePicker.allowsEditing = true
+                self.present(imagePicker, animated: true, completion: nil)
+                self.currentButton = sender
+            }
         
-        actionsheet.addAction(UIAlertAction(title: "Open Camera", style: UIAlertActionStyle.default, handler: { (action) -> Void in
-        }))
-        
-        actionsheet.addAction(UIAlertAction(title: "Choose Exisiting Photo", style: UIAlertActionStyle.default, handler: { (action) -> Void in
-        }))
-        actionsheet.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: { (action) -> Void in
-            
-        }))
-        myVc?.present(actionsheet, animated: true, completion: nil)
-        
-        
-        
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = .photoLibrary;
-            imagePicker.allowsEditing = true
-            self.present(imagePicker, animated: true, completion: nil)
-            currentButton = sender
-        }
     }
     
     // Bonus.
-    @IBAction func openCameraButton(_ sender: UIButton!) {
+     func openCameraButton(_ sender: UIButton!) {
         
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             let imagePicker = UIImagePickerController()
@@ -108,22 +97,38 @@ class ViewController: UIViewController,
         }
     }
     
+    // ouvre la pop up avec le choix camera ou photo librairie
+    @IBAction func selectPhotoButton(sender: UIButton!){
+        let actionsheet = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        actionsheet.addAction(UIAlertAction(title: "Open Camera", style: UIAlertActionStyle.default, handler: { (action) -> Void in
+            self.openCameraButton(sender)
+        }))
+        
+        actionsheet.addAction(UIAlertAction(title: "Choose Exisiting Photo", style: UIAlertActionStyle.default, handler: { (action) -> Void in
+            self.openButtonLibrairy(sender)
     
+    }))
+    actionsheet.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: { (action) -> Void in
     
+    }))
+    
+    self.present(actionsheet, animated: true, completion: nil)
+}
+    
+    //permet d afficher l image du boutton dans l image view
      func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let pickedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         print(pickedImage)
         currentButton!.setImage(pickedImage, for: .normal)
         currentButton!.imageView?.contentMode = .scaleAspectFill
         
-      //  monBoutton2.imageView?.image = pickedImage
-        
         dismiss(animated: true, completion: nil)
         
     }
-
+    
+    // ici on share une image
     @IBAction func swipePhotoView(_ sender: UISwipeGestureRecognizer) {
-        // ici on share une image
         let picture = UIImage(view: photoView)
         let activityVc = UIActivityViewController(activityItems:[picture] , applicationActivities: nil)
         activityVc.popoverPresentationController?.sourceView = self.view
